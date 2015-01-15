a46cf90e5f5c12142f8cc10a03695cb91dc42e6d
Jesus Arias Fisteus <jaf@it.uc3m.es>
2015-01-15 20:08:42 +0100

    First version of a relay that annotates some events.

---
+++ semserver/annotate.py
@@ -0,0 +1,121 @@
+from __future__ import unicode_literals, print_function
+
+import rdflib
+from rdflib.namespace import NamespaceManager
+
+import ztreamy.events
+import ztreamy.server
+import ztreamy.rdfevents
+
+class GenericAnnotator(object):
+
+    def __init__(self):
+        self.ns = {}
+        self.uri_ref_cache = {}
+        self.namespace_manager = NamespaceManager(rdflib.Graph())
+
+    def annotate_events(self, events):
+        annotated = []
+        for event in events:
+            annotated.extend(self.annotate_event(event))
+        return annotated
+
+    def annotate_event(self, event):
+        """Annotates the event and returns a resulting list of events.
+
+        Intended to be overriden by subclasses.
+
+        """
+        return event
+
+    def register_ns(self, key, uri_prefix, prefix=None):
+        self.ns[key] = rdflib.Namespace(uri_prefix)
+        if prefix:
+            self.namespace_manager.bind(prefix, self.ns[key])
+
+    def _create_graph(self):
+        return rdflib.Graph(namespace_manager=self.namespace_manager)
+
+    def _uri_ref(self, key, suffix):
+        if (key, suffix) in self.uri_ref_cache:
+            uri_ref = self.uri_ref_cache[(key, suffix)]
+        elif key in self.ns:
+            uri_ref = self.ns[key][suffix]
+        else:
+            uri_ref = self.ns[''][key + '#' + suffix]
+        return uri_ref
+
+    def _create_uri_ref(self, key, suffix):
+        uri_ref = self._uri_ref(key, suffix)
+        self.uri_ref_cache[(key, suffix)] = uri_ref
+        return uri_ref
+
+    def _create_uri_refs(self, data):
+        for key, suffix in data:
+            self._create_uri_ref(key, suffix)
+
+    def _create_event(self, event, graph):
+        return ztreamy.events.Event.create( \
+            event.source_id,
+            'text/n3',
+            graph,
+            application_id=event.application_id,
+            aggregator_id=event.aggregator_id,
+            event_type=event.event_type,
+            timestamp=event.timestamp,
+            extra_headers={'X-Derived-From': event.event_id})
+
+
+class DriverAnnotator(GenericAnnotator):
+
+    ns_hermes = 'http://webtlab.it.uc3m.es/ns/hermes/'
+
+    def __init__(self):
+        super(DriverAnnotator, self).__init__()
+        self.register_ns('', DriverAnnotator.ns_hermes, prefix='hermes')
+        self._create_uri_refs([
+            ('', 'averageSpeed'),
+            ('', 'efficiency'),
+            ('', 'latitude'),
+            ('', 'longitude'),
+            ('', 'averageHRM'),
+            ('', 'workLoad'),
+        ])
+
+    def annotate_event(self, event):
+        if not isinstance(event, ztreamy.events.JSONEvent):
+            return []
+        graph = self._create_graph()
+        measurement = self._uri_ref('measurement', event.event_id)
+        if 'averageHRM' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'averageHRM'),
+                       rdflib.Literal(event.body['averageHRM'])))
+        if 'averageSpeed' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'averageSpeed'),
+                       rdflib.Literal(event.body['averageSpeed'])))
+        if 'efficiency' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'efficiency'),
+                       rdflib.Literal(event.body['efficiency'].strip())))
+        if 'longitud' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'longitude'),
+                       rdflib.Literal(event.body['longitud'])))
+        if 'latitud' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'latitude'),
+                       rdflib.Literal(event.body['latitud'])))
+        if 'workLoad' in event.body:
+            graph.add((measurement,
+                       self._uri_ref('', 'workLoad'),
+                       rdflib.Literal(event.body['workLoad'].strip())))
+        return [self._create_event(event, graph)]
+
+
+class AnnotatedStream(ztreamy.server.RelayStream):
+    def __init__(self, path, streams, annotator, **kwargs):
+        kwargs['event_adapter'] = annotator.annotate_events
+        kwargs['parse_event_body'] = True
+        super(AnnotatedStream, self).__init__(path, streams, **kwargs)
---
+++ semserver/annotatingrelay.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals, print_function
+
+from ztreamy import StreamServer
+
+from . import annotate
+
+
+def main():
+    buffering_time = 1000
+    src_stream_uri = 'http://itaca.gast.it.uc3m.es:9100/events/priority'
+    server = StreamServer(9101)
+    annotator = annotate.DriverAnnotator()
+    stream = annotate.AnnotatedStream( \
+                            'annotated',
+                            [src_stream_uri],
+                            annotator,
+                            buffering_time=buffering_time)
+    server.add_stream(stream)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        server.stop()
+
+
+if __name__ == "__main__":
+    main()
