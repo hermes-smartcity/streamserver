bd7affd2a176346aeb7044e606fd7ecf4c56075b
Jesus Arias Fisteus <jaf@it.uc3m.es>
2015-05-11 18:00:30 +0200

    Bump version number to 0.3.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requirements = ['setuptools',
 
 setup(
     name = "hermes-semserver",
-    version = "0.2",
+    version = "0.3",
     author = "Jesus Arias Fisteus",
     description = ("A framework for publishing semantic events for HERMES"),
     keywords = "rdf sensors web semantic-sensor-web",
