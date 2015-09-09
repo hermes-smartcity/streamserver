ce5cac0ebe9e2d2e9c1b28498effcc3c4dca3355
Jesus Arias Fisteus <jaf@it.uc3m.es>
2015-09-09 13:13:51 +0200

    Improvements to the script that formats events in CSV

--- semserver/tools/driver_data.py
+++ semserver/tools/driver_data.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 import gzip
 import collections
-import sys
+import argparse
 
 import ztreamy
 
@@ -23,15 +23,28 @@ group_names = {
 }
 
 def read_file(filename):
+    events = []
     with gzip.GzipFile(filename, 'r') as f:
-        data = f.read()
-    return ztreamy.Deserializer().deserialize(data, complete=True)
+        for e in ztreamy.Deserializer().deserialize_file(f):
+            events.extend(e)
+    return events
 
-def clean(events):
-    events = events[7691:]
+def clean(events, from_date=None):
+    checked_events = []
     for event in events:
         if not event.timestamp.endswith('+02:00'):
             event.timestamp = event.timestamp + '+02:00'
+        try:
+            ztreamy.rfc3339_as_time(event.timestamp)
+        except ztreamy.ZtreamyException:
+            pass
+        else:
+            checked_events.append(event)
+    events = checked_events
+    if from_date:
+        from_date = ztreamy.rfc3339_as_time(from_date)
+        events = [e for e in events \
+                  if ztreamy.rfc3339_as_time(e.timestamp) >= from_date]
     return [e for e in events if e.application_id == 'SmartDriver']
 
 def group(events):
@@ -58,7 +71,8 @@ def write_groups(groups):
     all_data = []
     for name in group_names:
         data = []
-        for event in groups[name]:
+        for event in sorted(groups[name],
+                           key=lambda x: ztreamy.rfc3339_as_time(x.timestamp)):
             data.append(extract_data(event, name))
         with open(group_names[name] + '.dat', mode='w') as f:
             for row in data:
@@ -66,13 +80,24 @@ def write_groups(groups):
                 f.write('\n')
         all_data.extend(data)
     with open('all.dat', mode='w') as f:
-        for row in all_data:
+        for row in sorted(all_data,
+                          key=lambda x: ztreamy.rfc3339_as_time(x[1])):
             f.write(','.join([str(d) for d in row]))
             f.write('\n')
 
+def _parse_args():
+    parser = argparse.ArgumentParser(description='Generate CSV data files.')
+    parser.add_argument('events_file',
+                        help='gzipped events file')
+    parser.add_argument('-f', '--from-date', dest='from_date',
+                        default=None,
+                        help='only from the given RFC3339 date')
+    return parser.parse_args()
+
 def main():
-    filename = sys.argv[1]
-    groups = group(clean(read_file(filename)))
+    args = _parse_args()
+    groups = group(clean(read_file(args.events_file),
+                         from_date=args.from_date))
     write_groups(groups)
 
 if __name__ == "__main__":
