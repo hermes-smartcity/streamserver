1b893ce58d3011a03a44e765099f4bb6a2c849a9
Jesus Arias Fisteus <jaf@it.uc3m.es>
2016-07-20 12:43:47 +0200

    A minimum version of setuptools is needed by a dependency
    The dependency is html5lib, needed by ztreamy.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info[0] != 2 or sys.version_info[1] not in [7]:
     sys.exit(1)
 
 # Dependencies
-requirements = ['setuptools',
+requirements = ['setuptools>=18.5',
                 'ztreamy>=0.5',
                 ]
 
